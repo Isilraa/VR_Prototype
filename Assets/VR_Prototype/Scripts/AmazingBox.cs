d93af79dac5b86c20580af0ef4bba57ff49435db
Esteban <estebanhs91@hotmail.com>
2016-11-08 18:13:55 +0100

    Un bonito comienzo

---
+++ Assets/VR_Prototype/Scripts/AmazingBox.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class AmazingBox : MonoBehaviour, IGvrGazeResponder
+{
+    public void OnGazeEnter()
+    {
+        Debug.Log("entramos en la caja");
+    }
+
+    public void OnGazeExit()
+    {
+        Debug.Log("salimos de la caja");
+    }
+
+    public void OnGazeTrigger()
+    {
+        Destroy(gameObject);
+    }
+}
