f83a9bb9f9384a1be7971bea3eff22a12bb0c913
Isilraa <ardi.jpm@gmail.com>
2016-11-10 13:51:20 +0100

    Player Movement | Escena Menu

---
+++ Assets/LoadController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+
+public class LoadController : MonoBehaviour, IGvrGazeResponder {
+
+    private Animator animator;
+    public bool load;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnGazeEnter()
+    {
+        animator.SetBool("loading", true);
+    }
+
+    public void OnGazeExit()
+    {
+        animator.SetBool("loading", false);
+    }
+
+    public void OnGazeTrigger()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (load)
+            SceneManager.LoadScene("mainScene");
+    }
+}
